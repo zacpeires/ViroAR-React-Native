4543ec527d9a8821a743181f6c8f1d2d6fb66889
Zac Peires <zacpeires@googlemail.com>
2018-10-22 22:41:15 +0100

    features: added draggable smiley face and rotating

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -8,6 +8,14 @@ import {
   ViroARScene,
   ViroText,
   ViroConstants,
+  ViroBox,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroMaterials,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroAnimations
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -27,8 +35,22 @@ export default class HelloWorldSceneAR extends Component {
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-      </ViroARScene>
+      <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}}/>
+      <ViroAmbientLight color={"#aaaaaa"} />
+      <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
+        position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+      <ViroNode position={[0,-1,0]} dragType="FixedToWorld" onDrag={()=>{}} >
+        <Viro3DObject
+          source={require('./res/emoji_smile.vrx')}
+          resources={[require('./res/emoji_smile_diffuse.png'),
+              require('./res/emoji_smile_normal.png'),
+              require('./res/emoji_smile_specular.png')]}
+          position={[-.5, .5, -1]}
+          scale={[.2, .2, .2]}
+          type="VRX" />
+      </ViroNode>
+    </ViroARScene>
     );
   }
 
@@ -49,8 +71,25 @@ var styles = StyleSheet.create({
     fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
-    textAlign: 'center',  
+    textAlign: 'center',
   },
 });
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+ViroAnimations.registerAnimations({
+  rotate: {
+    properties: {
+      rotateY: "+=90"
+    },
+    duration: 250, //.25 seconds
+  },
+});
+
+
 
 module.exports = HelloWorldSceneAR;
